f75890ee959bc4b21572458e95b00b3b091a414e
EminHaziyev <mn05x.mail@gmail.com>
2025-01-12 02:09:09 +0400

    initial commit

---
+++ index.js
@@ -0,0 +1,138 @@
+require("dotenv").config();
+const mongoose = require("mongoose")
+const axios = require("axios");
+const puppeteer = require("puppeteer");
+
+mongoose.connect(process.env.MONGO);
+const siteSchema = {
+    name:{
+        type: String,
+        required: true
+    },
+    lastVacancy:{
+        type: String,
+        required: true
+    }
+}
+const Site = mongoose.model("Site", siteSchema);
+
+async function sendVacancy(siteName,vacancyName,vacancyHost,vacancyLink) {
+    const url = `https://api.telegram.org/bot${process.env.BOT_ID}/sendMessage`;
+    const message = `<i>Yeni vakansiya🆕</i>\n_____________\n<strong>${vacancyName}</strong> \n${vacancyHost}\n_____________\nVakansiya saytı: <b>${siteName}</b> \Keçid⬇️\n${vacancyLink}\n\nVacancyBoss ilə bütün vakansiyalardan anında xəbərdar olun: `;
+    try{
+        const response = await axios.post(url, {
+            chat_id: process.env.CHAT_ID,
+            text: message,
+            parse_mode: 'HTML'
+        });
+        const newRecord = await Site.findOneAndUpdate({name:siteName},{lastVacancy:vacancyName});
+    }
+    catch(err){
+        console.log("ERR while updating last vacancy",err);
+    }
+    
+    
+}
+//search_v2 because there are premium (on top) vacancies
+async function search_v2(siteName,siteUrl,siteVacancySelector,siteCreatorSelector,siteLinkSelector) {
+    const name = siteName;
+    const url = siteUrl;
+    try {
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+        await page.goto(url, { waitUntil: "networkidle2" });
+        
+        await page.waitForSelector(siteVacancySelector);
+        const newVacancyName = await page.$$eval(siteVacancySelector, elements => {
+
+            if (elements.length > 1) {
+                return elements[1].textContent.trim(); 
+            }
+            
+        });
+
+        const newVacancyCreator = await page.$$eval(siteCreatorSelector, elements => {
+      
+            if (elements.length > 1) {
+                return elements[1].textContent.trim(); 
+            }
+            
+        });
+        const newVacancyLink = await page.$$eval(siteLinkSelector, elements => {
+      
+            if (elements.length > 1) {
+                return elements[1].href; 
+            }
+            
+        });
+        const site = await Site.findOne({name});
+        
+        if(newVacancyName != site.lastVacancy){
+            
+            sendVacancy(siteName,newVacancyName,newVacancyCreator,newVacancyLink);
+        }
+        
+        await browser.close();
+    }
+    catch (err) {
+        console.log(err);
+    }
+}
+
+
+//search_v1 because there are no premium (on top) vacancies
+async function search_v1(siteName,siteUrl,siteVacancySelector,siteCreatorSelector,siteLinkSelector) {
+    const name = siteName;
+    const url = siteUrl;
+    try {
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+        await page.goto(url, { waitUntil: "networkidle2" });
+        
+        await page.waitForSelector(siteVacancySelector);
+        const newVacancyName = await page.$eval(siteVacancySelector, element => {
+
+                return element.textContent.trim(); 
+            
+        });
+
+        const newVacancyCreator = await page.$eval(siteCreatorSelector, element => {
+      
+            return element.nextSibling.textContent.trim();
+            
+        });
+        const newVacancyLink = await page.$eval(siteLinkSelector, element => {
+      
+            return element.href;
+            
+        });
+        const site = await Site.findOne({name});
+        
+        if(newVacancyName != site.lastVacancy){
+            
+            sendVacancy(siteName,newVacancyName,newVacancyCreator,newVacancyLink);
+        }
+        
+        await browser.close();
+    }
+    catch (err) {
+        console.log(err);
+    }
+}
+async function searchAll(){
+    await search_v1("busy.az","https://busy.az/vacancies?categories%5B%5D=12&categories%5B%5D=81&categories%5B%5D=82&categories%5B%5D=83&categories%5B%5D=84&categories%5B%5D=85&categories%5B%5D=86&categories%5B%5D=87&categories%5B%5D=88&categories%5B%5D=90&categories%5B%5D=91&categories%5B%5D=92&categories%5B%5D=93&categories%5B%5D=154&fullSelect=on&minimum_salary=&maximum_salary=",".job-listing-title",".job-listing-footer ul li i.icon-material-outline-business",".with-apply-button");
+    await delay(10000);
+    await search_v1("jobsearch.az","https://classic.jobsearch.az/vacancies?category=1076",".vacancies__title a",".vacancies__provided span",".vacancies__title a");
+    await delay(10000);
+    await search_v1("boss.az","https://boss.az/vacancies.mobile?search%5Bcategory_id%5D=38",".collection-i-link",".collection-i-company",".collection-i-link");
+    await delay(10000);
+    await search_v1("offer.az","https://www.offer.az/category/it-vakansiyalari/",".job-card__title",".job-card__meta em",".job-card__title");
+    await delay(10000);
+    await search_v1("ejob.az","https://ejob.az/is-elanlari/informasiya-texnologiyalari/",".job-card__title",".job-card__meta em",".job-card__title");
+    await delay(10000);
+    await search_v2("hellojob.az","https://www.hellojob.az/is-elanlari/texnologiya",".vacancies__desc h3",".vacancy_item_company",".vacancies__item");
+    await delay(10000);
+    await search_v2("smartjob.az","https://smartjob.az/vacancies?job_category_id[]=1",".brows-job-position h3 a",".company-title a",".brows-job-position h3 a");
+
+}
+setInterval(searchAll, 1000*60*4);
