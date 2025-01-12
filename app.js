d05332fcc1bb08c3e6b68f8942aae0677c4e06cc
Həziyev Emin <113848558+EminHaziyev@users.noreply.github.com>
2025-01-12 23:05:22 +0400

    U

--- app.js
+++ app.js
@@ -198,7 +198,7 @@ async function searchAll() {
 
         await search_v2("hellojob.az", "https://www.hellojob.az/is-elanlari/texnologiya", ".vacancies__desc h3", ".vacancy_item_company", ".vacancies__item");
 
-        await search_v2("smartjob.az", "https://smartjob.az/vacancies?job_category_id[]=1", ".brows-job-position h3 a", ".company-title a", ".brows-job-position h3 a");
+        // await search_v2("smartjob.az", "https://smartjob.az/vacancies?job_category_id[]=1", ".brows-job-position h3 a", ".company-title a", ".brows-job-position h3 a");
 
         await search_v3("busy.az", "https://busy.az/vacancies?categories%5B%5D=12&categories%5B%5D=81&categories%5B%5D=82&categories%5B%5D=83&categories%5B%5D=84&categories%5B%5D=85&categories%5B%5D=86&categories%5B%5D=87&categories%5B%5D=88&categories%5B%5D=90&categories%5B%5D=91&categories%5B%5D=92&categories%5B%5D=93&categories%5B%5D=154&fullSelect=on&minimum_salary=&maximum_salary=", ".job-listing-title", ".job-listing-footer ul li i.icon-material-outline-business", ".with-apply-button");
         console.log("All search functions executed");
@@ -209,7 +209,7 @@ async function searchAll() {
     }
 
 }
-searchAll();
+setInterval(searchAll, 1000*60*10);
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
