8e4e7b73cd94a9e7af37c5b68c825c18f08f475e
Həziyev Emin <113848558+EminHaziyev@users.noreply.github.com>
2025-01-18 16:29:08 +0400

    Update app.js

--- app.js
+++ app.js
@@ -251,7 +251,7 @@ async function searchAll() {
         
 
         await search_v3("busy.az", "https://busy.az/vacancies?categories%5B%5D=12&categories%5B%5D=81&categories%5B%5D=82&categories%5B%5D=83&categories%5B%5D=84&categories%5B%5D=85&categories%5B%5D=86&categories%5B%5D=87&categories%5B%5D=88&categories%5B%5D=90&categories%5B%5D=91&categories%5B%5D=92&categories%5B%5D=93&categories%5B%5D=154&fullSelect=on&minimum_salary=&maximum_salary=", ".job-listing-title", ".job-listing-footer ul li i.icon-material-outline-business", ".with-apply-button");
-        console.log("All search functions executed");
+        
     }
     catch (err) {
         console.log("Error in search functions", err);
